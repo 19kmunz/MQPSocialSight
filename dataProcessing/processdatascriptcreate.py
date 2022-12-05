2c8a892f5023b4a1577ec87a79cbc6d82f37575d
K Munz <kijamu@gmail.com>
2022-12-04 22:21:34 -0500

    - Python script: cd dataProcessing python .\processDataScriptUpdate.py there are Update and Create types. Create is more throughly tested Make sure you are have right .env file, it has db info - Backend: to run: npm start, go to localhost:3000 dao.js contains db stuff. make sure you have thw right windows environment variables set - Boxplots: in testingBoxPlot.html. directly in the script tags, some comments to point your way. Added the pre-processed documents directly in public.

--- dataProcessing/processdatascriptcreate.py
+++ dataProcessing/processdatascriptcreate.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
-
+from pymongo import MongoClient
+from dotenv import dotenv_values
 
 csvQuestionData = pd.read_table('MQPSocialMediaResponses.csv', sep=',', quotechar='"')
 media = ["Twitter", "Instagram", "Reddit", "Tumblr", "Tik Tok", "BeReal", "YouTube", "Snapchat", "Facebook", "4Chan",
@@ -18,47 +18,33 @@ for questionIndex in range(numberOfQuestions):
         mediaText = csvQuestionData.iloc[0][columnIndex+mediaIndex][len(questionText)+3:]
         # Get the header string to index the data
         questionExportTag = csvQuestionData.columns[columnIndex + mediaIndex]
-        print(questionExportTag)
 
         # Get the data for this question without nulls
         rawQuestionData = csvQuestionData[csvQuestionData[questionExportTag].notnull()].iloc[2:][questionExportTag].astype(int)
-
-        # Calculate Box Plot Info
-        min = np.amin(rawQuestionData)
-        max = np.amax(rawQuestionData)
-        median = np.median(rawQuestionData)
-        q3, q1 = np.percentile(rawQuestionData, [75, 25], method='midpoint')
-        iqr = q3 - q1
-        print(min, q1, median, q3, max, iqr)
-        if not q1.is_integer() or not q3.is_integer():
-            print(rawQuestionData.array)
+        arrQuestionData = rawQuestionData.sort_values().values.tolist()
 
         # Calculate Other Helpful Values
         total = rawQuestionData.size
         letterTag = questionExportTag[0]
-        numberTag = questionExportTag[0:questionExportTag.index("_")]
 
         questionObject = {
-            "questionExportTag": questionExportTag,
-            "questionText": questionText,
+            "_id": questionExportTag,
+            "qTag": questionExportTag,
+            "title": questionText,
             "mediaText": mediaText,
-            "min": min,
-            "q1": q1,
-            "median": median,
-            "q3": q3,
-            "max": max,
-            "iqr": iqr,
+            "human": letterTag,
             "total": total,
-            "letterTag" : letterTag,
-            "numberTag": numberTag
+            "points": arrQuestionData
         }
+        print(questionObject)
         # Add data for this question to data for the dataframe
         cleanQuestionData.append(questionObject)
 
 # Creates DataFrame.
-df = pd.DataFrame(cleanQuestionData)
-df.set_index("questionExportTag", inplace=True, verify_integrity=True)
+config = dotenv_values(".env")
 
-df.to_csv("MQPCleanData.csv")
+mongodb_client = MongoClient(config["ATLAS_URI"])
+database = mongodb_client[config["DB_NAME"]]
+collection = database[config["COLLECTION_NAME"]]
 
-# print(data.C1_1[2:].min())
+result = collection.insert_many(cleanQuestionData)
